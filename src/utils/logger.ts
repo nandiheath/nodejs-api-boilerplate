b4109a4c00832df057b6c5e4fdc5bfee2723fc12
Nandi Wong <nandi0315@gmail.com>
2019-03-21 11:51:31 +0800

    log to console only for running in container

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,24 +5,11 @@ const logger = winston.createLogger({
   level: LOGGER_LEVEL,
   format: winston.format.json(),
   transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
-  ]
+    new winston.transports.Console({
+      level: LOGGER_LEVEL,
+      format: winston.format.simple(),
+    }),
+  ],
 });
-
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    level: LOGGER_LEVEL,
-    format: winston.format.simple()
-  }));
-}
 
 export default logger;
